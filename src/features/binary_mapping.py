e89dd77cc2a61012554b477304cc1d9b8fcc057e
ananyahegde <hegdeananya613@gmail.com>
2025-06-21 18:05:44 +0530

    minor changes

--- src/features/binary_mapping.py
+++ src/features/binary_mapping.py
@@ -1,6 +1,9 @@
 class labelEncoder:
     def map_label(self, score):
-        if score <= 5:
+        if score <= 4:
             return 0  # negative
-        else:
+        elif score >= 7:
             return 1 # positive
+        else:
+            return 2 # Neutral
+        
